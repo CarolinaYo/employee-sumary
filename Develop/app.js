473e370d5f86f2373f9d19140aa8020b03f07b54
CarolY <mon_alisa75@yahoo.com>
2020-11-09 17:48:05 -0800

    removed async and resolved error in function addTeam.

--- Develop/app.js
+++ Develop/app.js
@@ -10,8 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-var keepAddingTeams = true;
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -131,7 +129,10 @@ function managerInfo() {
 
 }
 
-//need switch case to ask if they want to add new employee, if yes, need another switch case to ask if it is engineer or intern.
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
 
 function addTeam() {
   inquirer.prompt(addMemberOption).then(function (response) {
@@ -142,11 +143,12 @@ function addTeam() {
         if (err) {
           return console.log(err);
         }
-        console.log("Success!");
+        console.log("Your employee sumary has been created in team.html");
       });
     }
   });
 }
+
 
 function createTeamMember() {
   let employee;
@@ -178,60 +180,10 @@ function createTeamMember() {
   });
 }
 
-// function createTeamMember() {
-//     let newMemberRole = inquirer.prompt(memberRole);
-//     switch (newMemberRole.teamRole) {
-//       case "Engineer":
-//         let engAnswer = inquirer.prompt(engineerQuestions);
-//         let newEngineer = new Engineer(
-//           engAnswer.engname,
-//           engAnswer.engID,
-//           engAnswer.engEmail,
-//           engAnswer.github
-//         );
-//         teamMembers.push(newEngineer);
-//       case "Intern":
-//         let intAnswer = inquirer.prompt(internQuestions);
-//         let newIntern = new Intern(
-//           intAnswer.intname,
-//           intAnswer.intID,
-//           intAnswer.intEmail,
-//           intAnswer.school
-//         );
-//         teamMembers.push(newIntern);
-//     }
-// }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
 // function to initialize program
-
-async function init() {
+function init() {
   managerInfo();
-  
- 
-  
+  }
 
-  
-}
-
-//   function call to initialize program
+// function call to initialize program
 init();
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
